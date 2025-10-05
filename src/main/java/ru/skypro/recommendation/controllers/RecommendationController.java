2d4cdb11468da3a212d9c041400992e21116b918
ResistorD <resistord@gmail.com>
2025-10-05 13:49:43 +0200

    Продолжение работы + фиксы конфигурации

--- src/main/java/ru/skypro/recommendation/controllers/RecommendationController.java
+++ src/main/java/ru/skypro/recommendation/controllers/RecommendationController.java
@@ -3,6 +3,7 @@ package ru.skypro.recommendation.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skypro.recommendation.model.RecommendationDTO;
 import ru.skypro.recommendation.service.RecommendationService;
