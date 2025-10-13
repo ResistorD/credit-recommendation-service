4331f65a177abe3f2909571d5bbb681f25b7f51a
Dmitriy <dmitriyndubov@gmail.com>
2025-10-13 17:15:07 +0500

    Technical specifications for stage 2 - done

--- src/main/java/ru/skypro/recommendation/dto/RecommendationDTO.java
+++ src/main/java/ru/skypro/recommendation/dto/RecommendationDTO.java
@@ -12,6 +12,4 @@ public class RecommendationDTO {
     private String name;
     private String text;
 
-    public RecommendationDTO(String id, String name, String text) {
-    }
 }
--- src/main/resources/schema-rules.sql
+++ src/main/resources/schema-rules.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS rules (
     id UUID PRIMARY KEY,
     rule_name VARCHAR(255),
-    rule_description VARCHAR(1000)
+    rule_description VARCHAR(1000),
     rule_json TEXT,
     recommended_product_id (UUID)
 );
