3c43faf264bec38d6ce4b231b1ca76580c312493
ResistorD <resistord@gmail.com>
2025-10-19 18:55:10 +0200

    fix(management): восстановить содержимое Info/ManagementController из feat/management-endpoints; удалить пустые дубли

---
+++ src/main/java/ru/skypro/recommendation/controller/ManagementController.java
@@ -0,0 +1,23 @@
+package ru.skypro.recommendation.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.recommendation.repository.RecommendationRepository;
+
+@RestController
+@RequestMapping("/management")
+public class ManagementController {
+
+    private final RecommendationRepository recommendationRepository;
+
+    public ManagementController(RecommendationRepository recommendationRepository) {
+        this.recommendationRepository = recommendationRepository;
+    }
+
+    @PostMapping("/clear-caches")
+    public void clearCaches() {
+        // очищаем ручные Caffeine-кеши в репозитории
+        recommendationRepository.clearCaches();
+    }
+}
