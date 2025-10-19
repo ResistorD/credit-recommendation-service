2ba8532c3bcc226f760d4cec50666d515d95a183
xBananaKing79x <khakimov.t@gmail.com>
2025-10-19 19:11:32 +0300

    Создан новый объект БД для сохранения количества срабатываний в разрезе правил

---
+++ src/main/java/ru/skypro/recommendation/model/RuleStats.java
@@ -0,0 +1,45 @@
+package ru.skypro.recommendation.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "rule_stats")
+public class RuleStats {
+    @Id
+    private UUID ruleId; // ссылка на Rule.id
+
+    @Column(name = "hit_count")
+    private long hitCount = 0;
+
+    // Конструкторы
+    public RuleStats() {
+    }
+
+    public RuleStats(UUID ruleId, long hitCount) {
+        this.ruleId = ruleId;
+        this.hitCount = hitCount;
+    }
+
+    // Геттеры и сеттеры
+    public UUID getRuleId() {
+        return ruleId;
+    }
+
+    public void setRuleId(UUID ruleId) {
+        this.ruleId = ruleId;
+    }
+
+    public long getHitCount() {
+        return hitCount;
+    }
+
+    public void setHitCount(long hitCount) {
+        this.hitCount = hitCount;
+    }
+
+    public void increment() {
+        this.hitCount++;
+    }
+}
